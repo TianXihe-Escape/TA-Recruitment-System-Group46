03bc82801cce1944cb7dbd72325381c6030600a7
Xihe Tian <tianxihe56@gmail.com>
2026-03-28 18:02:58 +0800

    Update Main.java

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,1 +1,19 @@
-##
+package app;
+
+import service.DataService;
+import ui.LoginFrame;
+
+import javax.swing.*;
+
+/**
+ * Launches the stand-alone Swing application.
+ */
+public class Main {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            DataService dataService = new DataService();
+            dataService.ensureDataFiles();
+            new LoginFrame(dataService).setVisible(true);
+        });
+    }
+}
