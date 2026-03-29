e393d69ec360616683cd94f5f086cd560a162ce2
RBKcrazywind <1711353741@qq.com>
2026-03-29 15:56:19 +0800

    Update util package with new implementation

---
+++ src/main/java/util/JsonUtil.java
@@ -0,0 +1,26 @@
+package util;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+
+/**
+ * Provides the shared Jackson mapper configuration.
+ */
+public final class JsonUtil {
+    private static final ObjectMapper MAPPER = createMapper();
+
+    private JsonUtil() {
+    }
+
+    public static ObjectMapper getMapper() {
+        return MAPPER;
+    }
+
+    private static ObjectMapper createMapper() {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new JavaTimeModule());
+        mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        return mapper;
+    }
+}
